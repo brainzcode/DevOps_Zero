79065e6495f59a271e83539f4ac4a87e71e340e3
brainzcode <98150590+brainzcode@users.noreply.github.com>
2024-01-26 17:00:58 +0100

    backtick, shell-variables, system variables

---
+++ Day-7/shell_variables.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+var1=10
+
+var2=-57
+
+var3=testing
+
+var4="still more testing"
+
+# testing variables
+days=10
+
+guest="Imani"
+
+echo "$guest checked in $days days ago"
+
+days=5
+
+guest="Samuel"
+
+echo "$guest checked in $days days ago"
+
+# assigning a variable value to another variable
+value1=10
+
+value2=$value1
+
+echo "The resulting value is $value2"
---
+++ Day-7/system.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#System defined variables
+
+echo $SHELL
+echo $PWD
+echo $LOGNAME
+echo $HOME
+echo $PATH
+echo $SESSION_MANAGER
+~                        
